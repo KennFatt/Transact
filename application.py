99128d44e8880c2383b3ea8ea45fd493588a8860
KennFatt <clashofklen@gmail.com>
2020-04-17 12:30:38 +0700

    Initial workaround: Base Application template

---
+++ application.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter.ttk import *
+
+
+class Application(Tk):
+
+    def __init__(self):
+        Tk.__init__(self)
+
+
+if __name__ == "__main__":
+    Application().mainloop()
